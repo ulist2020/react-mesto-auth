9164b593c262cd158f51af4f63ef79c3672e3440
ulist2020 <75067604+ulist2020@users.noreply.github.com>
2021-05-25 12:54:43 +0500

    fix: делать верстку

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Route, Link, Switch } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__logo" />
+        <div className="header__menu">
+          <Switch>
+            <Route path="/sign-up">
+              <Link to="./sign-in" className="header__auth">
+                Войти
+              </Link>
+            </Route>
+            <Route path="/sign-in">
+              <Link to="./sign-up" className="header__auth">
+                Регистрация
+              </Link>
+            </Route>
+            <Route path="/">
+              <p className="header__email"></p>
+              <p className="header__auth" >
+                Выйти
+              </p>
+            </Route>
+          </Switch>
+        </div>
     </header>
   );
 }
